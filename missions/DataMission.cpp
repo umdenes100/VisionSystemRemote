fc24c601b957f8057f672648a9fed9cd04025364
Jet Lee <gamestar109@live.com>
2019-09-13 11:47:29 -0400

    Update and rename ChemicalMission.cpp to DataMission.cpp
    TODO: Figure out why Nick used bonusObjective instead of objective. I need to read more into it.

--- missions/DataMission.cpp
+++ missions/DataMission.cpp
@@ -21,3 +21,22 @@ QString ChemicalMission::objective(QString string) {
         break;
     }
 }
+
+#include "DataMission.h"
+
+#include <QString>
+
+DataMission::DataMission() : Mission()
+{
+}
+
+QString DataMission::bonusObjective(QString string) {
+    // first call is location
+    // later calls are ignored
+    if (bonus > 0) {
+        bonus--;
+        return "The Message is: " + string + "\n";
+    } else {
+        return "ERROR: too many base objective calls\n";
+    }
+}
