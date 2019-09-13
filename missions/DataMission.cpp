9c5ae252c1c87a6da163a2e8eeb1d349bbb9a582
Jet Lee <gamestar109@live.com>
2019-09-13 12:26:32 -0400

    Change bonusObjective to objective

--- missions/DataMission.cpp
+++ missions/DataMission.cpp
@@ -1,27 +1,3 @@
-#include "ChemicalMission.h"
-
-#include <QString>
-
-ChemicalMission::ChemicalMission() : Mission()
-{
-}
-
-QString ChemicalMission::objective(QString string) {
-    // first call is location
-    // later calls are ignored
-    switch(callNumber++) {
-    case 0:
-        return "The pH of the pool is: " + string + "\n";
-        break;
-    case 1:
-        return "The final pH of the pool is: " + string + "\n";
-        break;
-    default:
-        return "ERROR: too many mission() calls\n";
-        break;
-    }
-}
-
 #include "DataMission.h"
 
 #include <QString>
@@ -30,13 +6,15 @@ DataMission::DataMission() : Mission()
 {
 }
 
-QString DataMission::bonusObjective(QString string) {
+QString DataMission::objective(QString string) {
     // first call is location
     // later calls are ignored
-    if (bonus > 0) {
-        bonus--;
+    switch(callNumber++) {
+    case 0:
         return "The Message is: " + string + "\n";
-    } else {
-        return "ERROR: too many base objective calls\n";
+        break;
+    default:
+        return "ERROR: too many mission() calls\n";
+        break;
     }
 }
