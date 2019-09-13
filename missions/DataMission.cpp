6f06b88d1f86916d972730c4bf93b174057d1b56
Jet Lee <gamestar109@live.com>
2019-09-13 14:44:27 -0400

    Update comments in DataMission.cpp

--- missions/DataMission.cpp
+++ missions/DataMission.cpp
@@ -7,8 +7,10 @@ DataMission::DataMission() : Mission()
 }
 
 QString DataMission::objective(QString string) {
-    // first call is location
-    // later calls are ignored
+    /* 
+     * The first call is the character.
+     * Later calls are ignored.
+     */
     switch(callNumber++) {
     case 0:
         return "The Message is: " + string + "\n";
